f0717ca662ccc22d6a893b22fc110b431e8d2af8
kramarkirill <kkramar@kantiana.ru>
2020-09-20 17:19:50 +0200

    Initial Commit

---
+++ FireBase2/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FireBase2
+//
+//  Created by Кирилл Крамар on 20.09.2020.
+//  Copyright © 2020 Кирилл Крамар. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
